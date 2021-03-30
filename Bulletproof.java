831751f7933e39c43ba6e34f4624a8e9702feffd
Yazdan <yzdn.jt@gmail.com>
2021-03-31 01:10:45 +0430

    bulletproof class is completed. encapsulation and javadoc are added

--- Bulletproof.java
+++ Bulletproof.java
@@ -1,7 +1,19 @@
-public class Bulletproof extends VillagersGroup{
-    boolean isOneTimeHurt = false;
+public class Bulletproof extends VillagersGroup {
+    /**
+     * this is the special ability of this role .
+     * <p>
+     * in the night , if mafia try to kill him , he wont die for the first time!
+     * actually bulletproof has an extra-live in the night.
+     * but this extra-live can't help him in the day time!
+     */
+    public boolean isOneTimeHurt = false;
+
 
     public Bulletproof() {
+         /*
+          this is the only constructor of the bulletproof class.
+          once a Bulletproof object creates , NUMBER_OF_VILLAGERS will increase.
+         */
         NUMBER_OF_VILLAGERS++;
     }
 
