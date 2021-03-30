12174bc70b61facda84606b88b52af0cca95e8ac
Yazdan <yzdn.jt@gmail.com>
2021-03-31 00:15:35 +0430

    Role class is completed. encapsulation is implemented. documentes are added

--- Roles.java
+++ Roles.java
@@ -1,5 +1,15 @@
 public abstract class Roles {
-    boolean isMafia;
-    boolean isNightPlayer = false;
+
+    /**
+     * this property will be true if the player had an mafia role (mafia-silencer-godfather)
+     * if the player was a villager (villager-doctor-detective-bulletproof) will be false.
+     */
+    protected boolean isMafia;
+
+    /**
+     * some of the players can wake up during the night and do some special things!
+     * if the player could wake up in the night , this property will be ture!
+     */
+    protected boolean isNightPlayer;
 
 }
