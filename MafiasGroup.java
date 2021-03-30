bfa0ed489ce7370df1ded295114af14404ad4c16
Yazdan <yzdn.jt@gmail.com>
2021-03-31 00:46:28 +0430

    MafiasGroup is completed. encapsulation and additional comments added

--- MafiasGroup.java
+++ MafiasGroup.java
@@ -1,8 +1,13 @@
-public abstract class MafiasGroup extends Roles{
-    static int NUMBER_OF_MAFIAS = 0;
+public abstract class MafiasGroup extends Roles {
+    // this static int , will be count the number of "alive" mafias in the game
+    public static int NUMBER_OF_MAFIAS = 0;
 
-    protected MafiasGroup(){
+    protected MafiasGroup() {
+        // all of the mafias are in the mafiaGroup :)
         isMafia = true;
-        isNightPlayer =  true;
+
+        // all of the mafias can wake up in the night and vote.
+        // also some of them have special ability in the night!
+        isNightPlayer = true;
     }
 }
