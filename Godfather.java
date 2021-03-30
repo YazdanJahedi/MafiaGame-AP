708fc008ac762b2f6b3b3efe54e54aa546110c01
Yazdan <yzdn.jt@gmail.com>
2021-03-31 00:56:25 +0430

    Godfather is completed. additional comments are added

--- Godfather.java
+++ Godfather.java
@@ -1,9 +1,21 @@
-public class Godfather extends MafiasGroup{
+public class Godfather extends MafiasGroup {
 
     public Godfather() {
+         /*
+          this is the only constructor of the Mafia class.
+          once a Mafia object creates , NUMBER_OF_MAFIAS will increase.
+         */
         NUMBER_OF_MAFIAS++;
 
-        // this is the specific property of the godfather role .
+        /*
+         this is the specific property of the godfather role.
+
+         this is the point:
+            godfather is always child(sub-class) of the MafiasGroup .
+            but the "isMafia" property is false for him.
+
+            # actually if the detective checks him , he can't find out he is a mafia!
+         */
         isMafia = false;
     }
 
