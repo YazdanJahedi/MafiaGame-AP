d945a50621d4f37852ae220501e911b946369bda
Yazdan <yzdn.jt@gmail.com>
2021-03-31 00:29:46 +0430

    Villager class is completed. additional comments are added

--- Villager.java
+++ Villager.java
@@ -1,6 +1,10 @@
 public class Villager extends VillagersGroup{
 
     public Villager() {
+        /*
+          this is the only constructor of the Villager class.
+          once a villager object creates , NUMBER_OF_VILLAGERS will increase.
+         */
         NUMBER_OF_VILLAGERS++;
     }
 
